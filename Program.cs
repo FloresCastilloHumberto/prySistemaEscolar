a11c7c88d213ebdcdeca2985c7ac02d96c5232ee
Luis Gadiel Hernandez <lgbautistah12@gmail.com>
2026-06-24 10:34:23 -0600

    Programar los metodos Nuevo, Guardar y borrar para los botones de el frmCarreras.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace prySistemaEscolar
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            //Application.Run(new frmCarreras());
+            
             using (frmLogin login = new frmLogin())
             {
                 if (login.ShowDialog() == DialogResult.OK)
@@ -18,8 +20,6 @@ namespace prySistemaEscolar
                     Application.Run(new frmPrincipal());
                 }
             }
-                
-
         }
     }
 }
